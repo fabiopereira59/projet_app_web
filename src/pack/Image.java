3d9b6eb42f438f956bce1c905da020ac4d463e29
celiadjl <95174687+celiadjl@users.noreply.github.com>
2022-05-09 18:45:14 +0100

    Update Image.java

--- src/pack/Image.java
+++ src/pack/Image.java
@@ -1,8 +1,35 @@
 package pack;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Image {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id; 
+	String chemin;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getChemin() {
+		return chemin;
+	}
+	
+	public void setChemin(String chemin) {
+		this.chemin = chemin;
+	}
+	
+
+	
+	
 
 }
