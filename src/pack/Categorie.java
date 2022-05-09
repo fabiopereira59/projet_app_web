895808625e7b1128014e50973cbd31410d8a6ae3
celiadjl <95174687+celiadjl@users.noreply.github.com>
2022-05-09 18:44:16 +0100

    Update Categorie.java

--- src/pack/Categorie.java
+++ src/pack/Categorie.java
@@ -1,8 +1,31 @@
 package pack;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Categorie {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id; 
+	String nom;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }
