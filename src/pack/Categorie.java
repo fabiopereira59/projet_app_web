435f9b1c181347d7bc6487ee70225151ed9324d7
celiadjl <95174687+celiadjl@users.noreply.github.com>
2022-05-20 10:19:17 +0100

    Update Categorie.java

--- src/pack/Categorie.java
+++ src/pack/Categorie.java
@@ -1,9 +1,14 @@
 package pack;
 
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Categorie {
@@ -11,7 +16,21 @@ public class Categorie {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	int id; 
 	String nom;
+	@OneToMany(mappedBy="categorie", fetch=FetchType.EAGER, cascade = CascadeType.ALL , orphanRemoval = true)
+	Collection<Article> articles;
 	
+	public void addArticle(Article article) {
+		this.articles.add(article);
+	}
+	
+	public Collection<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(Collection<Article> articles) {
+		this.articles = articles;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -27,5 +46,6 @@ public class Categorie {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	
 }
