9e89c2c873304a832b3727a52049c0c8eddbdf05
celiadjl <95174687+celiadjl@users.noreply.github.com>
2022-05-20 10:15:46 +0100

    Create requetes.sql

---
+++ WebContent/requetes.sql
@@ -0,0 +1,19 @@
+INSERT INTO CATEGORIE VALUES ('0', 'Meubles');;
+INSERT INTO CATEGORIE VALUES ('1', 'Vêtements');;
+INSERT INTO CATEGORIE VALUES ('2', 'Livres');;
+INSERT INTO CATEGORIE VALUES ('3', 'Immobilier');;
+
+INSERT INTO ARTICLE VALUES ('0', 'Chaise rouge', 'celia.djilali@gmail.com', 'Chaise', '230', '0629174837', 'Toulouse','0');;
+INSERT INTO ARTICLE VALUES ('1', 'Canapé en cuir noir', 'celia.djilali@gmail.com', 'Canapé noir', '2000', '0629174837', 'Paris','0');;
+INSERT INTO ARTICLE VALUES ('2', 'Lavabo avec meuble', 'celia.djilali@gmail.com', 'Lavabo avec meuble', '230', '0629174837', 'Toulouse','0');;
+INSERT INTO ARTICLE VALUES ('3', 'Meuble de télévision', 'celia.djilali@gmail.com', 'Meuble de télévision', '2000', '0629174837', 'Paris','0');;
+INSERT INTO ARTICLE VALUES ('4', 'Rangement chaussure', 'celia.djilali@gmail.com', 'Rangement chaussure', '230', '0629174837', 'Toulouse','0');;
+INSERT INTO ARTICLE VALUES ('5', 'Canapé en cuir noir', 'celia.djilali@gmail.com', 'Bibliothèque', '2000', '0629174837', 'Paris','0');;
+INSERT INTO ARTICLE VALUES ('6', 'Table rouge ikea', 'celia.djilali@gmail.com', 'Façade lit', '230', '0629174837', 'Toulouse','0');;
+INSERT INTO ARTICLE VALUES ('7', 'Canapé en cuir noir', 'celia.djilali@gmail.com', 'Coiffeuse', '2000', '0629174837', 'Paris','0');;
+
+
+INSERT INTO ARTICLE VALUES ('8', 'Veste en jean', 'celia.djilali@gmail.com', 'Veste en jean', '30', '0629174837', 'Toulouse','1');;
+INSERT INTO ARTICLE VALUES ('9', 'Veste en cuir', 'celia.djilali@gmail.com', 'Veste en cuir', '200', '0629174837', 'Paris','1');;
+INSERT INTO ARTICLE VALUES ('10', 'Livre neuf', 'celia.djilali@gmail.com', 'Le Banquet de Platon', '5', '0629174837', 'Toulouse','2');;
+INSERT INTO ARTICLE VALUES ('3', 'Livre pour enfant', 'celia.djilali@gmail.com', 'Livre pour enfant', '3', '0629174837', 'Paris','2');;
